358a3aeb7e00898ed389c9e412bb9c961fac9550
Saurav Raj <118835139+Saurav-rajk@users.noreply.github.com>
2023-06-18 20:15:40 +0530

    Add files via upload

---
+++ Diamond pattern.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            int sc=n-i;
+            for(int j=1;j<=sc;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            int sc=n-i;
+            for(int j=1;j<=sc;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
